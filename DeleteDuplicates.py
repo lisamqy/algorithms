a9a5639536e52d7e2b72c8ef04482529e2b10e2f
Lisa Ma <lisamqy@gmail.com>
2021-10-07 22:19:47 -0700

    delete dupes from linked list

--- DeleteDuplicates.py
+++ DeleteDuplicates.py
@@ -12,16 +12,16 @@ class Solution:
         # if head exists, we return head(or if no head we return none by default)
         if head:
             current = head
-        # while current has a next...
-        while current.next:
-            # if current's val is same as the next's val
-            if current.val == current.next.val:
-                # reassign the next's val to the next next
-                current.next = current.next.next
-            # if not the same...
-            else:
-                # reassign current to the next to keep checking the rest of the list
-                current = current.next
+            # while current has a next...
+            while current.next:
+                # if current's val is same as the next's val
+                if current.val == current.next.val:
+                    # reassign the next's val to the next next
+                    current.next = current.next.next
+                # if not the same...
+                else:
+                    # reassign current to the next to keep checking the rest of the list
+                    current = current.next
         return head
 
 
